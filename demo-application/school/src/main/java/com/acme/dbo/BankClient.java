c3600be72e93194c376754ac3b0bb2e386699c80
nastyamorozova <borisenkina@gmail.com>
2021-07-08 22:51:39 +0300

    add proper logging and override equals

--- demo-application/school/src/main/java/com/acme/dbo/BankClient.java
+++ demo-application/school/src/main/java/com/acme/dbo/BankClient.java
@@ -1,9 +1,24 @@
 package com.acme.dbo;
 
+import java.util.Objects;
+
 public class BankClient {
     private String name;
 
     public BankClient(String name){
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BankClient that = (BankClient) o;
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
