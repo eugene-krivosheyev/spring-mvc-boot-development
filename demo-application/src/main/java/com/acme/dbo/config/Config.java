67dfd8347704107d5880cb639a70d37ee089083a
Eugene Krivosheev <eugene.krivosheev@gmail.com>
2021-07-11 23:09:39 +0300

    Demo application update: Introduce Boot. Refactor to simple Spring Boot application

--- demo-application/src/main/java/com/acme/dbo/config/Config.java
+++ demo-application/src/main/java/com/acme/dbo/config/Config.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan("com.acme.dbo")
-@PropertySource("classpath:application.properties")
 @EnableWebMvc
 public class Config {
     @Bean
