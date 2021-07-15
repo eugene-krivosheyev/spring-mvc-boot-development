adf5d7b5005909b3346dec374a5362d65c6efb9e
nastyamorozova <borisenkina@gmail.com>
2021-07-15 22:54:02 +0300

    Managing logging with profiled configuration

--- demo-application/school/src/test/java/com/acme/dbo/ClientIT.java
+++ demo-application/school/src/test/java/com/acme/dbo/ClientIT.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class ClientIT {
     @Autowired
     private MockMvc mockMvc;
